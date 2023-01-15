5fb30d93abc9e9e1b3ee0e7785ed1d8ef92bcfd3
junaid5546 <lafmk22@gmail.com>
2023-01-15 23:14:14 +0400

    added api

--- src/app/components/add-item/add-item.component.ts
+++ src/app/components/add-item/add-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoriesService, Icategory, IchildSubCat, IsubCategory,TypeSizeService } from 'api-package';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -7,13 +8,48 @@ import { Observable } from 'rxjs';
   styleUrls: ['./add-item.component.scss'],
 })
 export class AddItemComponent implements OnInit {
-  
-  constructor() { }
+  public allCategories: Observable<Icategory[]> | undefined;
+  public allSubCategories: Observable<IsubCategory[]> | undefined;
+  public childSubCategories:Observable<IchildSubCat[]> | undefined;
+  public itemAvailableSize:Observable<any>|undefined;
 
-  ngOnInit() {}
+  constructor(private category:CategoriesService, private typeSize:TypeSizeService) { }
 
-  getSubCategory =()=> {}
-  getCategories =()=>  {}
-  getSubCatChild =()=> {}
+  ngOnInit() {
+    this.getCategories();
+  }
+
+  getCategories = async()=>  {
+    this.allCategories = await this.category.getAllCategories();
+    this.allCategories.subscribe((res:any)=>{
+      console.log(res)
+    })
+  }
+
+  getSubCategory = async (categoryId:string)=> {
+    this.allSubCategories = await this.category.getSubCategoryByCategoryId(categoryId);
+  }
+
+  getSubCatChild = async (subCatId:string) => {
+    this.childSubCategories = await this.category.getChildBySubCategoryId(subCatId);
+  }
+
+  /**
+   * ON SELECTING CATEGORY THIS FUNCTION GETS CALLED
+   * USING ID- MONGODB ID AND SETTING IT FOR CALLING SUB-CATEGORY.
+   */
+  onSelectCategory = (event:any):void => {
+    let value:string = event.detail.value;
+    // GETTING SUB CATEGORIES OF ITEM.
+    this.getSubCategory(value);
+  }
+  /**
+   * ON SELECTING CATEGORY THIS FUNCTION GETS CALLED
+   * USING ID- MONGODB ID AND SETTING IT FOR CALLING SUB-CATEGORY.
+   */
+  onSelectSubCat(event:any):void{
+    let value:string = event.detail.value;
+    this.getSubCatChild(value);
+  }
 
 }
--- src/app/services/add-item-service/add-item.service.ts
+++ src/app/services/add-item-service/add-item.service.ts
@@ -1,41 +1,38 @@
 import { Injectable } from '@angular/core';
-import { CategoriesService, Icategory, IchildSubCat, IsubCategory } from 'api-package';
+import { CategoriesService, Icategory, IchildSubCat, IsubCategory,TypeSizeService } from 'api-package';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AddItemService {
-  allCategories: Observable<Icategory[]> | undefined;
-  allSubCategories: Observable<IsubCategory[]> | undefined;
-  childSubCategories:Observable<IchildSubCat[]> | undefined;
-  itemAvailableSize:Observable<any>|undefined;
+  private allCategories: Observable<Icategory[]> | undefined;
+  private allSubCategories: Observable<IsubCategory[]> | undefined;
+  private childSubCategories:Observable<IchildSubCat[]> | undefined;
+  private itemAvailableSize:Observable<any>|undefined;
   
-  constructor(private category:CategoriesService) { }
+  constructor(private category:CategoriesService, private typeSize:TypeSizeService) { }
 
   // RETURNS THE LIST OF CATEGORY
-  getAllCategories = async() => {
+  getAllCategories = async():Promise<Observable<Icategory[]>> => {
     this.allCategories =  (await this.category.getAllCategories());
+    return this.allCategories;
     }
 
     // RETURNS THE LIST OF SUB-CATEGORY.
-  getSubCategoryByCategoryId = async (categoryId:string) => {
+  getSubCategoryByCategoryId = async (categoryId:string):Promise<Observable<IsubCategory[]>> => {
     this.allSubCategories = (await this.category.getSubCategoryByCategoryId(categoryId));
+    return this.allSubCategories;
   }
 
   // RETURN LIST OF CHILDREN.
-  getChildOfSubCat = async (subCategoryId:string) => {
+  getChildOfSubCat = async (subCategoryId:string):Promise<Observable<IchildSubCat[]>> => {
     this.childSubCategories = (await this.category.getChildBySubCategoryId(subCategoryId));
-    this.childSubCategories?.subscribe((res:any)=>{
-      console.log("CHILD", res)
-    })
+    return this.childSubCategories;
   }
 
   getAvailableSize = async(child_cat_id:string,type:string) => {
-    this.itemAvailableSize = (await this.category.getAvailableSize(child_cat_id,type));
-    this.itemAvailableSize?.subscribe((res:any)=>{
-      console.log("RES available size: ",res);;
-    })
+    this.itemAvailableSize = (await this.typeSize.getAvailableSize(child_cat_id,type));
   }
 
 }
