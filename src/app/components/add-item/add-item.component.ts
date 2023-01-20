6c4adbd0b44fedc8260a5821e3e263c223ca178c
junaid5546 <lafmk22@gmail.com>
2023-01-21 00:11:18 +0400

    changes

--- src/app/components/add-item/add-item.component.ts
+++ src/app/components/add-item/add-item.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { CategoriesService, Icategory, IchildSubCat, IsubCategory,UtilService } from 'api-package';
+import { CategoriesService, Cloth, Icategory, IchildSubCat, IsubCategory,Item,UtilService } from 'api-package';
 import { IsizeType } from 'api-package/lib/interfaces/sizeType';
 import { Observable } from 'rxjs';
+import { size } from '../../../fake-response/size';
 
 @Component({
   selector: 'app-add-item',
@@ -15,13 +16,16 @@ export class AddItemComponent implements OnInit {
   public allCategories: Observable<Icategory[]> | undefined;
   public allSubCategories: Observable<IsubCategory[]> | undefined;
   public childSubCategories:Observable<IchildSubCat[]> | undefined;
-  public itemAvailableSize:Observable<any>|undefined;
+  public itemAvailableSize:IsizeType[]|undefined;
+  public itemAvailableColor:Observable<any>|undefined;
   public sizeType:Observable<IsizeType[]>|undefined;
+  private myFashion:Cloth = new Cloth();
 
   constructor(private category:CategoriesService, private typeSize:UtilService) { }
 
   ngOnInit() {
     this.getCategories();
+    this.getColorsList();
   }
 
   getCategories = async()=>  {
@@ -41,24 +45,34 @@ export class AddItemComponent implements OnInit {
    * USING ID- MONGODB ID AND SETTING IT FOR CALLING SUB-CATEGORY.
    */
   onSelectCategory = (event:any):void => {
+    console.log(event)
     let id:string = event.detail.value.id;
-    console.log("Event",event)
+    let name = event.detail.value.name;
     // GETTING SUB CATEGORIES OF ITEM.
     this.isGenderBased = event.detail.value.genderBased;
     this.getSubCategory(id);
     this.getSizeType();
+    this.myFashion.setItemCatId(id);
+    this.myFashion.setItemCategoryName("en",name.en)
+    this.myFashion.setItemCategoryName("ar",name.ar)
+    console.log("ITEM ", this.myFashion)
   }
   /**
    * ON SELECTING CATEGORY THIS FUNCTION GETS CALLED
    * USING ID- MONGODB ID AND SETTING IT FOR CALLING SUB-CATEGORY.
    */
   onSelectSubCat(event:any):void{
-    let value:string = event.detail.value;
-    this.getSubCatChild(value);
+    let subCategory = event.detail.value;
+    let id:string = event.detail.value.id;
+    this.myFashion.setItemSubCatId(id);
+    this.myFashion.setItemSubCategoryName("en",subCategory.name.en);
+    this.myFashion.setItemSubCategoryName("ar",subCategory.name.ar);
+    this.getSubCatChild(id);
+    
   }
 
   getSizeBasedOnType = async (child_cat_id:string,type:string) => {
-    this.itemAvailableSize = (await this.typeSize.getAvailableSize(child_cat_id,type));
+    this.itemAvailableSize = size;// (await this.typeSize.getAvailableSize(child_cat_id,type));
   }
   getSizeType = async () => {
     this.sizeType = (await this.typeSize.getTypes());
@@ -66,11 +80,22 @@ export class AddItemComponent implements OnInit {
 
   onSelectChildSubCat=(event:any)=>{
     this.childCategoryId = event.detail.value.id;
+    let itemName = event.detail.value.name;
+    console.log("ITEM NAME: ", itemName)
+    this.myFashion.setItemId(this.childCategoryId);
+    this.myFashion.setItemSubCategoryChildName("en",itemName.en);
+    this.myFashion.setItemSubCategoryChildName("ar",itemName.ar);
+    console.log("**ITEM ", this.myFashion)
   }
 
   onSelectGenderType=(event:any)=>{
     this.typeId = event.detail.value.name.en;
     this.getSizeBasedOnType(this.childCategoryId,this.typeId);
+  }
+
+  getColorsList = async () => {
+    this.itemAvailableColor = (await this.typeSize.getColors());
+   
   }
 
 }
---
+++ src/fake-response/size.ts
@@ -0,0 +1,24 @@
+import { IsizeType } from "api-package/lib/interfaces/sizeType";
+
+export const size:IsizeType[] = [
+  {
+    id: '63a9b958d38789473ba91a23',
+    name: { en: '6-12 M', ar: '6-12 M' },
+  },
+  {
+    id: '63a9b958d38789473ba91a23',
+    name: { en: '1-2 Y', ar: '1-2 y' },
+  },
+  {
+    id: '63a9b958d38789473ba91a23',
+    name: { en: '2-3 Y', ar: '2-3 Y' },
+  },
+  {
+    id: '63a9b958d38789473ba91a23',
+    name: { en: '3-4 Y', ar: '3-4 Y' },
+  },
+  {
+    id: '63a9b958d38789473ba91a23',
+    name: { en: '4-5 Y', ar: '4-5 Y' },
+  },
+];
